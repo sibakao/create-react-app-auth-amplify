91d239c22104cf6c75ddc3110d2ca9f69ec65a83
sibakaoru <sibakao@gmail.com>
2023-04-11 20:14:01 +0900

    test:VBStudio更新テスト

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
-import { View, Text, StyleSheet} from 'react-native';
+//import { View, Text, StyleSheet} from 'react-native';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      current: new Date().toLocaleString()
-    };
-  }
+  //constructor(props) {
+  //  super(props);
+  //  this.state = {
+  //    current: new Date().toLocaleString()
+  //  };
+  //}
   render() {
     return (
       <div className="App">
@@ -22,7 +22,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           </header>
           <h1>Sample App</h1>
-          <p>これは{this.state.current}にWeb版VisualStudioで修正したアプリです。</p>
+          <p>これは0411にWeb版VisualStudioで修正したアプリです。</p>
       </div>
     );
   }
